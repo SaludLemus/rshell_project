27219607699204f1e4afdfd8ce74bff6f824e416
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-10-27 12:54:19 -0700

    Added class implementation

---
+++ src/user.cpp
@@ -0,0 +1,26 @@
+#include "user.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+User::User() : cmd_controller(0) {}
+
+User::~User() {
+  if (cmd_controller) // ensure proper deallocation
+      delete cmd_controller;
+}
+
+void User::setCommand() {
+   string user_input; // store command
+
+   cout << "$ ";
+   getline(cin, user_input); // ask user for input
+   
+  if (cmd_controller) // dealloc. old command
+		delete cmd_controller;
+ 
+	cmd_controller = new Command();
+
+	cmd_contoller->init(user_input); // set parser's data member
+   return;
+}
