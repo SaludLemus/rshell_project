f76ea4474a655c5158b851e957ae83b413d2df25
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-10-27 17:43:11 -0700

    Added implementation methods

---
+++ src/command.cpp
@@ -0,0 +1,40 @@
+#include "command.h"
+
+Command::Command() : parse_cmd(0) {}
+
+Command::~Command() {
+	deallocParser();
+	deallocCMDList();
+}
+
+void Command::init(const string &user_input) {
+	deallocParser();
+	parse_cmd = new Parse();
+	
+	parse_cmd->str.setString(user_input); // set Tokenizer's buffer
+	
+	commandIterator(); // start parse
+	return;
+}
+
+void Command::commandIterator() {
+	deallocCMDList();
+	
+	while (parse_cmd->nextParse() != "") // create commands from user's input
+		cmd_list.push_back(new CommandLine(parse_cmd->current_parse));
+	// loop through the list and execute command or exit
+	return;
+}
+
+void Command::deallocParser() {
+	if (parse_cmd)
+		delete parse_cmd;
+	return;
+}
+
+
+void Command::deallocCMDList() {
+	// for(...)
+		// delete ...
+	return;
+}
