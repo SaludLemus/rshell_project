51f703725c5fe2f5d87b36da057b0cdd657eae76
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-10-27 17:40:05 -0700

    Added deallocation methods

--- src/command.h
+++ src/command.h
@@ -5,16 +5,21 @@
 #include "commandline.h"
 #include <iostream>
 #include <string>
+#include <list>
 using namespace std;
 
 class Command {
    public:
       void init(const string &);
    private:
+		Command();
+		~Command();
       Parse* parse_cmd;
       List<CommandLine*> cmd_list;
-      void commandIterator();
+      void commandIterator(); // will call exit() if "exit" exists or end of parse is reached
       void exit();
+      void deallocParser();
+      void deallocCMDList();
 };
 
 #endif
