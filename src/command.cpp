687d0dcd9959414e560728857dfe104c4ca2fdf5
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-11-02 19:25:20 -0700

    Added argv for execvp

--- src/command.cpp
+++ src/command.cpp
@@ -21,37 +21,30 @@ void Command::commandIterator() {
 	CommandLine* new_cmd = 0;
 	// use parse_cmd
 	do {
-		new_cmd = parse_cmd->nextParse();
-		cout << new_cmd->getCommand() << endl;
+		new_cmd = parse_cmd->nextParse(); // get command
 		cmd_list.push_back(new DefaultCommand(new_cmd));
-		if (cmd_list.size() >= 2) {
-			cout << ".." << cmd_list.at(cmd_list.size() - 2)->getCMD() << endl;
-		cout << ".." << cmd_list.at(cmd_list.size() - 1)->getCMD() << endl;
-		}
-		cout << "Connector: " << cmd_list.at(cmd_list.size() - 1)->getConnector() << endl;
+
 		if (cmd_list.at(cmd_list.size() - 1)->getConnector() == 0)
 			break;
 	} while (cmd_list.size() != 0 && cmd_list.at(cmd_list.size() - 1)->getConnector() != 0); // continue parsing until last command is reached or no input is entered
 	
-	cout << "size of vector: " << cmd_list.size() << endl;
-	cout << "CMD at index 0: " << cmd_list.at(0)->getCMD() << endl;
 	for (unsigned int i = 0; i < cmd_list.size(); ++i) { // iterate through each cmd
-	//	if (cmd_list.at(i)->getConnector() == 0) { // exitCC
-		//	cmd_list.at(i)->execute();
-		//	exitProg();
-	//	}
-	//	else if (cmd_list.at(i)->getConnector() == 1) // continueCC
-	//		cmd_list.at(i)->execute();
-	//	else if (cmd_list.at(i)->getConnector() == 2) {// andCC
-	//		cmd_list.at(i)->execute();
-	//		if (cmd_list.at(i)->checkStatus() == false) // skip next cmd
-	//			++i;
-	//	}
-		//else if (cmd_list.at(i)->getConnector() == 3) // orCC
-		//	cmd_list.at(i)->execute();
-		//	if (cmd_list.at(i)->checkStatus() == true) // skip next cmd
-		//		++i;
-		cout << cmd_list.at(i)->getCMD() << endl; // null term.
+		if (cmd_list.at(i)->getConnector() == 0) { // exitCC
+			cmd_list.at(i)->execute();
+			exitProg();
+		}
+		else if (cmd_list.at(i)->getConnector() == 1) // continueCC
+			cmd_list.at(i)->execute();
+		else if (cmd_list.at(i)->getConnector() == 2) {// andCC
+			cmd_list.at(i)->execute();
+			if (cmd_list.at(i)->checkStatus() == false) // skip next cmd
+				++i;
+		}
+		else if (cmd_list.at(i)->getConnector() == 3) // orCC
+			cmd_list.at(i)->execute();
+			if (cmd_list.at(i)->checkStatus() == true) // skip next cmd
+				++i;
+		// cout << cmd_list.at(i)->getCMD() << endl; // null term.
 	}
 	
 	return;
--- src/defaultcommand.cpp
+++ src/defaultcommand.cpp
@@ -1,5 +1,8 @@
 #include "defaultcommand.h"
 #include <unistd.h>
+#include <sys/wait.h>
+#include <iostream>
+using namespace std;
 DefaultCommand::DefaultCommand() : Connector(), exec_command(0) {}
 
 DefaultCommand::DefaultCommand(CommandLine* new_cmd) : Connector(), exec_command(new_cmd) {}
@@ -9,6 +12,79 @@ void DefaultCommand::execute() {
 			// do the sys calls here, if succeed set cmdSuccess to true, else false
 		pid_t child_pid;
 		int child_status;
+		
+		// initiate arg list.
+		// execvp(CMD, arg list) e.g. cd is CMD and backend is arg list; arg list must bell null term.
+		char* argv[];
+		int index = 0;
+		
+		char* cmd_to_execute = exec_command->getCommand();
+		int argument_size = 0;
+		
+		int i = 0;
+		
+		while (true) {
+			if (cmd_to_execute[i] == ' ')
+				++argument_size;
+			else if (cmd_to_execute[i] == '\0')
+				++argument_size;
+		}
+		
+		char* argv[argument_size];
+		char* new_arg = new char[];
+		argument_size = 0;
+		int new_arg_index = 0;
+		
+		while (true) { // cd a
+			if (cmd_to_execute[i] != ' ') {
+				new_arg[new_arg_index] = cmd_to_execute[i];
+				++new_arg_index;
+			}
+			else if (cmd_to_execute[i] == ' ') {
+				argv[index] = new_arg; // add to argument list for execvp
+				++index;
+			
+				new_arg_index = 0; // start over
+				if (cmd_to_execute[i + 1] != '\0')
+					new_arg = new char[];
+			}
+			else if (cmd_to_execute[i] == '\0') {
+				argv[index] = new_arg;
+				++index;
+				break;
+			}
+			++i;
+		}
+		
+		if (index > 0)
+			argv[index] = NULL; // null term.
+		else {
+			cout << "Error in execute()" << endl;
+			return;
+		}
+		
+		child_pid = fork(); // create child process
+		
+		if (child_pid == 0) { // child will run cmd
+			cmdSuccess = true;
+			
+			execvp(argv[0], argv);
+			
+			// if continues, then failed (unknown cmd)
+			
+			cout << "Unknown command." << endl;
+			
+			cmdSuccess = false;
+			
+		}
+		else { // parent has to wait for child to be done
+			pid_t check_pid = waitpid(child_pid, &child_status);
+			do {
+				if (check_pid != child_pid) return; // need to fix
+			} while(check_pid != child_pid);
+			return child_status;
+		}
+			
 	}
 	return;
 }
