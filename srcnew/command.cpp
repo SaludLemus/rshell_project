dacbb39b398d29dc5204a58872de4d5bb8624277
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-11-11 06:36:39 -0800

    Added execvp

--- srcnew/command.cpp
+++ srcnew/command.cpp
@@ -1,6 +1,12 @@
 #include "command.h"
 #include <iostream>
-
+#include <errno.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+using namespace std;
 Command::Command(){
     commandArray = 0;
 }
@@ -14,6 +20,47 @@ Command::~Command(){
 }
 
 bool Command::execute(){
+	if (commandArray== 0) // points to char* that is mem. loc. 0
+		return false;
+		
+	pid_t child_pid; // for fork()
+	int child_status; // for waitpid()
+		
+	//checkExit(commandArray[0]); // exit prog if exit exists
+	child_pid = fork(); // create child process fork() returns an integer (0 == child)
+		
+	if (child_pid == 0) { // child will run cmd
+		
+		if (execvp(commandArray[0], commandArray) < 0) {// returns a negative value if failed to execute
+			perror("ERROR: Unknown command"); // error
+			//cout << "ERROR in child process returning to parent process" << endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+		
+	else if (child_pid == -1) {// fork failed
+		perror("ERROR: Unable to fork a child process"); // error message
+		exit(EXIT_FAILURE);
+	}
+	else { // parent has to wait for the child to be done
+		pid_t check_pid = waitpid(child_pid, &child_status, 0);
+		do {
+			
+			if (errno == EINTR) {// will set errno to EINTR if waitpid returns -1
+				perror("ERROR: Function was interrupted");
+				exit(EXIT_FAILURE);
+				return false;
+			}
+			else if (WIFEXITED(child_status)) { // child process did not execute
+				if (WEXITSTATUS(child_status) == EXIT_FAILURE) {
+					//cout << "returning false" << endl;
+					return false;
+				}
+			}
+				
+		} while(check_pid != child_pid);
+	}
+	//cout << "returning true" << endl;
     return true;
 }
 
