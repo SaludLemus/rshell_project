c7e9e2aa59b354e716aaaa365a35bfb07d3410c7
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-11-12 22:34:06 -0800

    Handle error of stat()

--- src/test.cpp
+++ src/test.cpp
@@ -19,8 +19,22 @@ Test::~Test(){
 bool Test::execute(){
 	struct stat flag_handler; // for -e, -f, -d
 		
-	// handle error -1
-	//if (stat()
+	// commandArray[0] is the flag type; commandArray[1] is the file path	
+		
+	
+	if (stat(commandArray[1], &flag_handler) == -1) { // handle error -1, errno
+		if (errno == EACCES) // search permission is denied in the path
+			perror("Search permission is denied for one of the directories.");
+		else if (errno == ELOOP) // too many symbolic links while traversing
+			perror("Too many symbolic links while traversing.");
+		else if (errno == ENAMETOOLONG) // path is too long
+			perror("Path is too long.");
+		else if (errno == ENOENT) // path is empty or component of path DNE
+			perror("Component of path does not exits.");
+		else if (errno == ENOTDIR) // component of path prefix of path is not a dir.
+			perror("Component of path is not a directory.");
+		exit(EXIT_FAILURE); // error
+	}
 	// determine flag type 
 
 	// -e
