a8ef7bd2555a47d5b0cbac133ff86dbe5130e3e5
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-11-12 22:54:28 -0800

    Added flag cases

--- src/test.cpp
+++ src/test.cpp
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+using namespace std;
 Test::Test(){
     return;
 }
@@ -18,10 +19,10 @@ Test::~Test(){
 
 bool Test::execute(){
 	struct stat flag_handler; // for -e, -f, -d
-		
+	bool stat_success = true;
 	// commandArray[0] is the flag type; commandArray[1] is the file path	
 		
-	
+	// stat(const char* path, struct stat* buf)
 	if (stat(commandArray[1], &flag_handler) == -1) { // handle error -1, errno
 		if (errno == EACCES) // search permission is denied in the path
 			perror("Search permission is denied for one of the directories.");
@@ -33,27 +34,49 @@ bool Test::execute(){
 			perror("Component of path does not exits.");
 		else if (errno == ENOTDIR) // component of path prefix of path is not a dir.
 			perror("Component of path is not a directory.");
-		else 
+		else // handle all other errors
 			perror("Error, file/directory specified can not be completed.");
+		stat_success = false;
 		exit(EXIT_FAILURE); // error
 	}
-	// determine flag type 
-
-	// -e
-		// if true, print true
-		// else false
 	
-	// -f
-		// if true, print true
-		// else false
+	char* flag_type = commandArray[0]; // determine flag type 
 	
-	// -d
-		// if true, print true
+	if (flag_type[1] == 'e') { // -e case
+		if (stat_success) { // file/directory exists
+				cout << "(True)" << endl;
+				return true;
+		}
+		else
+			return false;
+	}
+	
+	else if (flag_type[1] == 'f') { 	// -f case
+		// S_ISREG(mode_t) returns non-zero int if true
+		if (S_ISREG(flag_handler.st_mode) != 0) {
+			cout << "(True)" << endl;
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	else if (flag_type[1] == 'd') { // -d case
+		// S_ISDIR(mode_t) returns non-zero int if true
+		if (S_ISDIR(flag_handler.st_mode) != 0) {
+			cout << "(True)" << endl;
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	else {// flag_type[1] does not match any of the flags
+		cout << "Flag type does not match" << endl;
+		exit(EXIT_FAILURE); // error
+	}
 		
-		// else false
-		
-    return true;
-	
+    return false;
 }
 
 void Test::display(){
