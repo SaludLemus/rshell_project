311bf27c85a28fd1512be3cd99b6fc54b82c5d97
Jose Garcia <jgarc129@ucr.edu>
2017-11-16 16:48:58 +0000

    Adding comments to the functions of parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -45,43 +45,52 @@ Base* Parser::getRoot(){
 	return root;
 }
 
+// A parser that specifically looks for commands, parathesis, and special commands
+// Returns a Base* of the next command it sees, starting from the position
 Base* Parser::nextCommand(){
-	// Check for special cases
+	// Check for special commands (i.e exit, test, parathesis)
 	Base* spcCMD = returnSpecialCommand();
 	if (spcCMD != NULL)
 		return spcCMD;
 
-	// tracks the last position of a word
+	// The parser uses two position trackers:
+	// startingposition is the position at the beginning of the word
+	// endposition is the position at the end of the word
+	 
+	size_t startingposition = position;
 	size_t endposition = position;
-	// first tracks the starting position of a word then a modified last position
-	size_t backtrackposition = position;
-	// parameter size
+	
+	// Execvp function requires a char* array, as such, the array size needs to be 
+	// calculated beforehand
 	int parameterSize = 0;
 		
 	// Get command array size
 	do{
-		backtrackposition = endposition;
-		endposition = str.string::find(" ", endposition); // returns string::npos if not found
-	}while(!checkCharSize(endposition, backtrackposition, parameterSize));
+		startingposition = endposition;
+		endposition = str.string::find(" ", endposition); /* CHANGE TO A BETTER WORD SEARCH LATER */
+	}while(!checkCharSize(endposition, startingposition, parameterSize));
 	
 	// Create Parameter array
 	char** commands = new char*[parameterSize + 1];
 		
-	// tracks the last position of a word
+	// The parser has two additional position trackers
+	// parsedendposition is the position at the end of the word, separate of the
+	// endposition variable (endposition is used to set the position for next parse)
+	// backtrackposition is a modified position at the end of the word, used
+	// so the substring ignores ';' and ')'
+	size_t backtrackposition = position;
 	size_t parsedendposition = position;
-	// tracks the starting position of a word
-	size_t initalposition = position;
 	
 	// Fill Parameter with char*'s
 	for(int i = 0; i < parameterSize; i++){
-		initalposition = parsedendposition;
+		startingposition = parsedendposition;
 		backtrackposition = parsedendposition;
-		parsedendposition = str.string::find(" ", parsedendposition); // returns string::npos if not found
+		parsedendposition = str.string::find(" ", parsedendposition); /* CHANGE TO A BETTER WORD SEARCH LATER */
 		returnEndForParameters(parsedendposition, backtrackposition);
-		commands[i] = stringToCharStar(str.string::substr(initalposition, backtrackposition - initalposition));
+		commands[i] = stringToCharStar(str.string::substr(startingposition, backtrackposition - startingposition));
 	}
 	
-	// set the last array to null
+	// set the last array index to null
 	commands[parameterSize] = NULL;
 	
 	// Update position for next parse
@@ -90,22 +99,25 @@ Base* Parser::nextCommand(){
 	return new Command(commands);
 }
 
+// A parser that specifically looks for connector
+// Returns a Connector* of the next connector it sees, starting from the position
 Connector* Parser::nextConnector(){
 	size_t startPosition = position;
-	position = str.string::find(" ", position); // returns string::npos if not found
+	position = str.string::find(" ", position); // sets the position for the next parse
 	
-	//std::cout << "Position[" << startPosition << "|" << position << "]" << std::endl;
-	
+	// This never seems to happen
 	if (position == string::npos){
 		std::cout << "Error with parsing. Did you leave extra spaces?" << std::endl;
 		return NULL;
 	}
 	
+	// Return Semicolon* if it sees, well, a semicolon
 	if (str[startPosition] == ';'){
 		position++;
 		return new Semicolon();
 	}
 	
+	// Return an And* or Or* if either of the strings are found
 	string connector = str.string::substr(startPosition, 2);
 	if (connector == "&&"){
 		position++;
@@ -116,15 +128,16 @@ Connector* Parser::nextConnector(){
 		return new Or();
 	}
 	
-	std::cout << "Error with parsing at connector. This shouldn't happen." << std::endl;
-	std::cout << "Connector string check [" << connector << "]" << std::endl;
-	std::cout << "Semicolon check [" << str[startPosition] << "]" << std::endl;
-	std::cout << "Position[" << startPosition << "|" << position << "]" << std::endl;
+	std::cout << "Error with parsing at connector. Did you leave extra spaces?." << std::endl;
 	return NULL;
 }
 
+// A helper parser of nextCommand that specifically looks for special commands
+// Should never be used by itself, use nextCommand() instead
+// Returns a Base* of the next special command it sees, starting from the position
+// Returns NULL if no special command is found
 Base* Parser::returnSpecialCommand(){
-	// check parathesis
+	// Returns a Base* if the first letter is an open parathesis "("
 	if(str[position] == '('){
 		position++;
 		return returnTreeCommand();
@@ -132,7 +145,7 @@ Base* Parser::returnSpecialCommand(){
 	
 	string command = str.string::substr(position, 4);
 	
-	// check exit
+	// Returns an Exit* if the exectuable is specifically exit
 	if (command == "exit"){
 		size_t backtrackposition = position;
 		size_t endposition = str.string::find(" ", position);
@@ -143,30 +156,30 @@ Base* Parser::returnSpecialCommand(){
 		return new Exit();
 	}
 	
-	// check if test (test version)
+	// Returns a Test* if the exectuable is specifically test
 	if (command == "test"){
 		// mirrors the procedure of the original parser, but slightly modified
+		size_t startingposition = position;
 		size_t endposition = position;
-		size_t backtrackposition = position;
 		int parameterSize = 0;
 			
 		do{
-			backtrackposition = endposition;
-			endposition = str.string::find(" ", endposition); // returns string::npos if not found
-		}while(!checkCharSize(endposition, backtrackposition, parameterSize));
+			startingposition = endposition;
+			endposition = str.string::find(" ", endposition);
+		}while(!checkCharSize(endposition, startingposition, parameterSize));
 		
 		char** commandArray = new char*[3];
 		
 		size_t parsedendposition = position;
-		size_t initalposition = position;
+		size_t backtrackposition = position;
 		int i = 0;	//index
 		
 		// skip once for test
 		backtrackposition = parsedendposition;
-		parsedendposition = str.string::find(" ", parsedendposition); // returns string::npos if not found
+		parsedendposition = str.string::find(" ", parsedendposition);
 		returnEndForParameters(parsedendposition, backtrackposition);
 		
-		// parameter size = 2 (autofill to -e)
+		// autofill to -e if the parameter size is 2
 		if (parameterSize == 2){
 			commandArray[i] = stringToCharStar("-e");
 			i++;
@@ -174,11 +187,11 @@ Base* Parser::returnSpecialCommand(){
 		
 		// fill last two (or one) spots
 		while(i < 2){
-			initalposition = parsedendposition;
+			startingposition = parsedendposition;
 			backtrackposition = parsedendposition;
 			parsedendposition = str.string::find(" ", parsedendposition); // returns string::npos if not found
 			returnEndForParameters(parsedendposition, backtrackposition);
-			commandArray[i] = stringToCharStar(str.string::substr(initalposition, backtrackposition - initalposition));
+			commandArray[i] = stringToCharStar(str.string::substr(startingposition, backtrackposition - startingposition));
 			i++;
 		}
 		
@@ -191,28 +204,27 @@ Base* Parser::returnSpecialCommand(){
 		return new Test(commandArray);
 	}
 	
-	// check if test ([] verstion)
+	// Returns a Test* if the first letter is an open bracket "["
 	if (str[position] == '['){
 		// mirrors the procedure of the original parser, but slightly modified
-		
 		position += 2; // move position cursor, past [
 		
+		size_t startingposition = position;
 		size_t endposition = position;
-		size_t backtrackposition = position;
 		int parameterSize = 0;
 			
 		do{
-			backtrackposition = endposition;
-			endposition = str.string::find(" ", endposition); // returns string::npos if not found
-		}while(!checkCharSize(endposition, backtrackposition, parameterSize));
+			startingposition = endposition;
+			endposition = str.string::find(" ", endposition);
+		}while(!checkCharSize(endposition, startingposition, parameterSize));
 		
 		char** commandArray = new char*[3];
 		
 		size_t parsedendposition = position;
-		size_t initalposition = position;
+		size_t backtrackposition = position;
 		int i = 0;	//index
 		
-		// parameter size = 1 (autofill to -e)
+		// autofill to -e if the parameter size is 2
 		if (parameterSize == 1){
 			commandArray[i] = stringToCharStar("-e");
 			i++;
@@ -220,11 +232,11 @@ Base* Parser::returnSpecialCommand(){
 		
 		// fill last two (or one) spots
 		while(i < 2){
-			initalposition = parsedendposition;
+			startingposition = parsedendposition;
 			backtrackposition = parsedendposition;
 			parsedendposition = str.string::find(" ", parsedendposition); // returns string::npos if not found
 			returnEndForParameters(parsedendposition, backtrackposition);
-			commandArray[i] = stringToCharStar(str.string::substr(initalposition, backtrackposition - initalposition));
+			commandArray[i] = stringToCharStar(str.string::substr(startingposition, backtrackposition - startingposition));
 			i++;
 		}
 		
@@ -237,8 +249,9 @@ Base* Parser::returnSpecialCommand(){
 		return new Test(commandArray);
 	}
 	
-	// check if empty
-	if (position == str.length()){
+	// Returns an empty command if the position overextends the parser
+	// Should never be reached as the function would break before reaching here
+	if (position >= str.length()){
 		return new Command();
 	}
 	
@@ -267,12 +280,12 @@ Base* Parser::returnTreeCommand(){
 
 // Updates the endposition and parameter size. 
 // Returns true when it reaches an end
-bool Parser::checkCharSize(size_t & endposition, size_t backtrackposition, int & parameterSize) {
+bool Parser::checkCharSize(size_t & endposition, size_t startingposition, int & parameterSize) {
 	if (endposition == string::npos){
 		endposition = str.length();
 	}
 	
-	char firstLetter = str[backtrackposition];
+	char firstLetter = str[startingposition];
 	
 	// Check for comments
 	if (firstLetter == '#'){
@@ -304,9 +317,9 @@ bool Parser::checkCharSize(size_t & endposition, size_t backtrackposition, int &
 	}
 	
 	// Check for an alone && ||
-	string connector = str.string::substr(backtrackposition, 2);
+	string connector = str.string::substr(startingposition, 2);
 	if(connector == "&&" || connector == "||"){
-		endposition = backtrackposition;
+		endposition = startingposition;
 		return true;
 	}
 	
