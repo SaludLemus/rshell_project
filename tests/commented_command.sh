8bdf597a853dae3ca2861bcfe5b9db50b137da5e
Jose Garcia <jgarc129@ucr.edu>
2017-11-18 01:43:34 +0000

    Updated comment test

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -5,18 +5,8 @@ TEST1="pwd #display directory"
 TEST2="ls # -a"
 TEST3="ls -a #; pwd"
 TEST4="ls -a; pwd # || && ; echo ; sd"
-TEST5="echo hello world && FAILCOMMAND how are you doing && echo I'm #doing great"
+TEST5="echo hello world && FAILCOMMAND how are you doing || echo I'm #doing great"
 TEST6="#Just a comment"
+TEST7="exit"
 
-echo -e "Test 1: $TEST1"
-echo -e $TEST1 | ./bin/rshell
-echo -e "\nTest 2: $TEST2"
-echo -e $TEST2 | ./bin/rshell
-echo -e "\nTest 3: $TEST3"
-echo -e $TEST3 | ./bin/rshell
-echo -e "\nTest 4: $TEST4"
-echo -e $TEST4 | ./bin/rshell
-echo -e "\nTest 5: $TEST5"
-echo -e $TEST5 | ./bin/rshell
-echo -e "\nTest 6: $TEST6"
-echo -e $TEST6 | ./bin/rshell
+echo -e "$TEST1\n$TEST2\n$TEST3\n$TEST4\n$TEST5\n$TEST6\n$TEST7\n" | ./bin/rshell
