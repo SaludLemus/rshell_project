3338d1c860765656e1cdea53cb53d83e82ccde5b
Jose Garcia <jgarc129@ucr.edu>
2017-12-05 03:30:40 +0000

    Added input redirection test

--- tests/input_redirect_test.sh
+++ tests/input_redirect_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #tests input redirection commands
 
-TEST1="cat < Makefile"
-TEST2="cat < Makefile;"
-TEST3="cat < Makefile | echo sup"
-TEST4=""
-TEST5=""
-TEST6=""
-TEST7=""
-TEST8=""
+TEST1="cat < B" #prints B
+TEST2="cat A < B;" #prints A
+TEST3="cat < A B A" #prints B A
+TEST4="cat A B A < A" #prints A B A
+TEST5="cat A B A < A B A" #prints A B A B A
+TEST6="echo < B" #prints nothing
+TEST7="echo A < B" #prints A
+TEST8="echo A B A < A B A" #prints A B A B A
 TEST9="exit"
 
 echo -e "$TEST1\n$TEST2\n$TEST3\n$TEST4\n$TEST5\n$TEST6\n$TEST7\n$TEST8\n$TEST9\n" | ./bin/rshell
