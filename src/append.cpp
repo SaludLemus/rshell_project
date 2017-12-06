6f418b8119712bdd1c5e0d4a9c0ebcc7507b208a
Salud Lemus <slemu003@hammer.cs.ucr.edu>
2017-12-05 19:23:47 -0800

    Added flag

--- src/append.cpp
+++ src/append.cpp
@@ -18,33 +18,55 @@ bool Append::execute(){
 	if (!file_name)
 		file_name = rightNode->getCommand();
 	
-	if (leftNode && rightNode) { // child exists
-		int save_1 = dup(1); // save [1]
+	int save_1 = dup(1); // save [1]
+	
+	//check dup()
+	if (!check_dup(save_1))
+		return false;
+		
+	int save_file_fd = open(file_name, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU); // set fd for file
+	
+	if (save_file_fd == -1) // open() failed
+		return false;
+	
+	dup2(save_file_fd, 1); // change fd for file to [1]
+	
+	close(save_file_fd); // close fd for file
+	
+	if (!leftNode->execute())
+		return false;
+	
+	dup2(save_1, 1); // change what [1] was back to [1]
+	
+	close(save_1); // close fd that was opened
+	
+	//if (leftNode && rightNode) { // child exists
+		//int save_1 = dup(1); // save [1]
 		
 		// check dup()
-		if (!check_dup(save_1))
-			return false;
+		//if (!check_dup(save_1))
+			//return false;
 		
 		// check close() and close [1]
-		if (!check_close())
-			return false;
+		//if (!check_close())
+			//return false;
 		
 		// set fd for file to [1] via open()
-		if (!change_output())
-			return false;
+		//if (!change_output())
+			//return false;
 		
 		// MIGHT HAVE TO WRITE A NEWLINE TO FILE BEFORE CHILD'S EXECUTE() IS CALLED
 		
 		// check execute()
-		if (!leftNode->execute()) // child failed
-			return false;
+		//if (!leftNode->execute()) // child failed
+			//return false;
 
 		// restore save_1 and check
-		if (!restore_save1(save_1))
-			return false;
+		//if (!restore_save1(save_1))
+			//return false;
 			
-		return true;
-	}
+		//return true;
+	//}
 	return false;
 }
 
